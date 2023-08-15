13a274853ccf661058d5d727604ae50af5186fe9
Axioms <atomic.axioms@gmail.com>
2023-08-14 21:48:27 -0400

    code clean up

--- feather-tft/code.py
+++ feather-tft/code.py
@@ -7,18 +7,21 @@ BMP_PADDING = 8
 LABEL_HEIGHT = 40
 BMP_HEIGHT = 70
 TOTAL_ENCODERS = 5
-TIME_INTERVAL = 0.001 
+TIME_INTERVAL = 0.001
 labels = []
 bmps = []
 bmp_inversion_matrix = []
 main_window = displayio.Group()
 label_group = displayio.Group()
 bmp_group = displayio.Group()
-# init UART                                                                                                             
+# init UART
 uart = busio.UART(rx=board.RX, baudrate=1000000,timeout=0)
 
 def padString(string, pad, delim = " "):
-    if len(string) < pad:
+    strlen = len(string)
+    if strlen > pad:
+        string = string[0:pad]
+    elif strlen < pad:
         delta = pad - len(string)
         string = (delim * delta) + string
     return string
@@ -27,7 +30,7 @@ display = board.DISPLAY
 
 for encoder_index in range(TOTAL_ENCODERS):
     # create Text Label
-    tmp_tabel = bitmap_label.Label(terminalio.FONT, scale=2, x=((BMP_WIDTH + BMP_PADDING) * encoder_index), y=LABEL_HEIGHT, text=padString(str(encoder_index * 10), 3))
+    tmp_tabel = bitmap_label.Label(terminalio.FONT, scale=2, x=((BMP_WIDTH + BMP_PADDING) * encoder_index), y=LABEL_HEIGHT, text=padString(str(-1), 3))
     labels.append(tmp_tabel)
     label_group.append(tmp_tabel)
     # create image
@@ -46,16 +49,17 @@ def ChangeVolume(encoder, volume):
     labels[encoder].text = padString(str(volume), 3)
 
 def ToggleMute(encoder):
-    bmp_inversion_matrix[encoder] = not bmp_inversion_matrix[encoder] 
+    bmp_inversion_matrix[encoder] = not bmp_inversion_matrix[encoder]
     tmp_bmp = displayio.OnDiskBitmap("images/" + str(encoder) + "." + ("1" if bmp_inversion_matrix[encoder] else "0") + ".bmp")
     bmp_display = displayio.TileGrid(tmp_bmp, pixel_shader=tmp_bmp.pixel_shader, x=((BMP_WIDTH + BMP_PADDING) * encoder), y=BMP_HEIGHT)
     bmp_group[encoder] = bmp_display
 
 def SeedValues(data):
-    seed_index = data_string.index("Seed Values: ")
-    values = data_string[(seed_index + 13):].split(",")
+    seed_index = data.index("Seed Values: ")
+    values = data[(seed_index + 13):].split(",")
     for value in range(len(values)):
         labels[value].text = padString(values[value], 3)
+        time.sleep(1)
 
 while True:
     text = uart.readline()
